60ffdb94b0fb0bffe3507068a057c246299d7387
Saul Marino <saulcolima417@aragon.unam.mx>
2021-08-20 23:09:42 -0500

    eligios changes

---
+++ eligios_changes/eligios_changes.js
@@ -0,0 +1,2 @@
+
+console.log("Eligos Changes");
---
+++ eligios_changes/eligios_changes.php
@@ -0,0 +1,14 @@
+<?php 
+echo "Hola estos son los cambios de eligio"; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Eligio changes </title>
+
+</head>
+<body>
+	<h1>Hola estos son los cambios hechos por eligio</h1>
+</body>
+</html>
